fbb097468c6688852b3b7b4ab12bf518662407a9
orvino <91889241+orvino@users.noreply.github.com>
2022-02-01 19:45:48 -0600

    7.3.1

--- schema.sql
+++ schema.sql
@@ -35,9 +35,9 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dep_emp(
-	dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -45,14 +45,77 @@ CREATE TABLE dep_emp(
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE titles(
-	emp_no INT NOT NULL,
-	titles VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+CREATE TABLE titles (
+ 	emp_no INT NOT NULL,
+ 	title VARCHAR() NOT NULL,
+ 	from_date DATE NOT NULL,
+ 	to_date DATE NOT NULL,
+ 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+ 	PRIMARY KEY (emp_no),
+	UNIQUE (title)
 );
 
-SELECT * FROM departments;
+CREATE TABLE titles (
+    emp_no int  NOT NULL ,
+    title varchar  NOT NULL ,
+    from_date date  NOT NULL ,
+    to_date date  NOT NULL ,
+    CONSTRAINT pk_title PRIMARY KEY (
+        emp_no,title,from_date
+		)
+	);
 
+SELECT * FROM titles;
+
+-- The SELECT statement is more specific this time. Instead of an asterisk to indicate that we want all of the records, we're requesting only the first and last names of the employees.
+--FROM employees tells SQL in which of the six tables to look.
+--The WHERE clause brings up even more specifics. We want SQL to look in the birth_date column for anyone born between January 1, 1952, and December 31, 1955.
+--Notice how BETWEEN and AND are both capitalized in our statement? This is part of the SQL syntax. It not only signals the conditions present, but also makes the code easier to read.
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring with Count
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Saving table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+
+
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
